36e1f01fdf0c835bd2d1485bd536d8fd407ae317
Andreas Brain <dev@abrain.de>
2020-07-15 14:40:10 +0200

    Get the known resources based on their alternative name identifiers

--- src/flowcontrol.ts
+++ src/flowcontrol.ts
@@ -1,10 +1,10 @@
-import {Application} from "./declarations";
+import {Application, ResourceIdentifierData} from "./declarations";
 import logger from "./logger";
 
 export default function (app: Application) {
   let textAnalysisService = app.service('textanalysis');
   // Register for results from the OCR service
-  app.service('ocr').on('ocr_result', text => {
+  app.service('ocr').on('ocr_result', async text => {
     let result
     try {
       result = textAnalysisService.analyse(text)
@@ -14,5 +14,22 @@ export default function (app: Application) {
     }
 
     logger.debug('Text analysis completed', result)
+
+    // Determine the requested resources
+    const resourceIds = new Set();
+    const ResourceIdentifierService = app.service('resource-identifiers');
+    for (const name of result.resources) {
+      const identifiers = await ResourceIdentifierService.find({ query: { type: 'name', value: name, $limit: 1 }, paginate: false }) as ResourceIdentifierData[]
+      if (identifiers.length === 0) {
+        logger.warn('Unknown resource: %s', name)
+        continue;
+      }
+
+      resourceIds.add(identifiers[0].resourceId)
+    }
+
+    let ResourceService = app.service('resources')
+    let resources = await ResourceService.find({ query: { id: { $in: Array.from(resourceIds.values()) } }, paginate: false });
+    logger.debug(resources)
   })
 }
