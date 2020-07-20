3098839db847c3a20b1f37076e6621607179d05b
Andreas Brain <dev@abrain.de>
2020-07-20 16:17:48 +0200

    Create a location object from the raw data, for now only a simple string used as fallback

--- src/services/locations/locations.class.ts
+++ src/services/locations/locations.class.ts
@@ -1,9 +1,38 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
-import {Application, LocationData} from '../../declarations';
+import {Application, LocationData, RawLocation} from '../../declarations';
+import {DataTypes} from "sequelize";
 
 export class Locations extends Service<LocationData> {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
+  }
+
+  createFromRawLocation(rawLocation: RawLocation): Promise<LocationData> {
+    let fallbackAddress = this.getFallbackAddress(rawLocation)
+
+    let location = {
+      rawText: fallbackAddress,
+      latitude: undefined,
+      longitude: undefined,
+      street: '',
+      number: '',
+      detail: '',
+      postCode: '',
+      city: '',
+      country: ''
+    };
+
+    return this.create(location) as Promise<LocationData>
+  }
+
+  /**
+   * Try to generate a readable address out of the given raw data.
+   *
+   * @param rawLocation
+   */
+  getFallbackAddress(rawLocation: RawLocation): string {
+    // For now we assume the address format used in Germany
+    return `${rawLocation.street || ''} ${rawLocation.streetnumber || ''}\n${rawLocation.zip || ''} ${rawLocation.city || ''}`
   }
 }
