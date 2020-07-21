792b73d3f478353bfeb61527cbf9d4414d54f57d
Andreas Brain <dev@abrain.de>
2020-07-21 17:36:57 +0200

    Add basic address resolution by OSM's Nominatim

--- src/services/locations/locations.class.ts
+++ src/services/locations/locations.class.ts
@@ -1,17 +1,20 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import {Application, LocationData, RawLocation} from '../../declarations';
-import {DataTypes} from "sequelize";
+import {Nominatim, NominatimResult} from './nominatim.class';
+import logger from '../../logger';
 
 export class Locations extends Service<LocationData> {
+  private nominatim: Nominatim
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
+    this.nominatim = new Nominatim()
   }
 
-  createFromRawLocation(rawLocation: RawLocation): Promise<LocationData> {
+  async createFromRawLocation(rawLocation: RawLocation): Promise<LocationData> {
     let fallbackAddress = this.getFallbackAddress(rawLocation)
 
-    let location = {
+    let location: LocationData = {
       rawText: fallbackAddress,
       latitude: undefined,
       longitude: undefined,
@@ -23,7 +26,19 @@ export class Locations extends Service<LocationData> {
       country: ''
     };
 
-    return this.create(location) as Promise<LocationData>
+    try {
+      location = await this.validateWithNominatim(location)
+    } catch (error) {
+      logger.warn('Error while validating the location with Nominatim, using raw values', error.message || error)
+
+      // Just use the raw values
+      location.street = rawLocation.street
+      location.number = rawLocation.streetnumber
+      location.postCode = rawLocation.zip
+      location.city = rawLocation.city
+    }
+
+    return await this.create(location) as LocationData
   }
 
   /**
@@ -34,5 +49,29 @@ export class Locations extends Service<LocationData> {
   getFallbackAddress(rawLocation: RawLocation): string {
     // For now we assume the address format used in Germany
     return `${rawLocation.street || ''} ${rawLocation.streetnumber || ''}\n${rawLocation.zip || ''} ${rawLocation.city || ''}`
+  }
+
+  async validateWithNominatim(data: LocationData): Promise<LocationData> {
+    const results = await this.nominatim.geocode(data.rawText)
+    let bestResult = await this.getBestResult(results)
+
+    // Enhance the location data with bits from the Nominatim response
+    data.street = bestResult.address.road
+    data.number = bestResult.address.house_number
+    data.postCode = bestResult.address.postcode
+    data.city = bestResult.address.village || bestResult.address.town || bestResult.address.city || bestResult.address.municipality || ''
+    data.country = bestResult.address.country_code === 'de' ? 'Deutschland': ''
+    data.latitude = Number(bestResult.lat)
+    data.longitude = Number(bestResult.lon)
+
+    return data
+  }
+
+  async getBestResult(results: NominatimResult[]): Promise<NominatimResult> {
+    if (!Array.isArray(results) || results.length === 0) {
+      throw new Error('No results were given')
+    }
+
+    return results[0]
   }
 }
---
+++ src/services/locations/nominatim.class.ts
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import logger from '../../logger';
+
+export interface NominatimResult {
+  place_id: bigint,
+  licence: string,
+  osm_type: string,
+  osm_id: bigint,
+  boundingbox: [],
+  lat: string,
+  lon: string,
+  display_name: string,
+  class: string,
+  type: string,
+  importance: number,
+  address: {
+    house_name?: string,
+    house_number: string,
+    road: string,
+    isolated_dwelling?: string,
+    croft?: string,
+    hamlet?: string,
+    subdivision?: string,
+    suburb?: string,
+    borough?: string,
+    district?: string,
+    city_district?: string,
+    village?: string,
+    town?: string,
+    city?: string,
+    municipality?: string,
+    county: string,
+    state: string,
+    postcode: string,
+    country: string,
+    country_code: string
+  }
+}
+
+export class Nominatim {
+  private baseUrl: URL
+
+  constructor(baseUrl?: URL) {
+    this.baseUrl = baseUrl || new URL('https://nominatim.openstreetmap.org')
+  }
+
+  geocode(search: string): Promise<NominatimResult[]> {
+    return axios.get('/search', {
+      baseURL: this.baseUrl.toString(),
+      params: {
+        'accept-language': 'de',
+        addressdetails: 1,
+        countrycodes: 'de',
+        format: 'json',
+        q: search
+      },
+      responseType: 'json'
+    })
+      .then(result => {
+        logger.debug(result.data)
+        return result.data as NominatimResult[]
+      })
+  }
+}
