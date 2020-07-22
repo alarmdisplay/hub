ec2b589d078b783066a858260d88654dee855096
Andreas Brain <dev@abrain.de>
2020-07-22 20:13:31 +0200

    Include location and alerted resources when querying alerts

--- src/services/alerts/alerts.hooks.ts
+++ src/services/alerts/alerts.hooks.ts
@@ -1,4 +1,5 @@
 import * as authentication from '@feathersjs/authentication';
+import {HookContext} from "@feathersjs/feathers";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
@@ -6,8 +7,8 @@ const { authenticate } = authentication.hooks;
 export default {
   before: {
     all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
+    find: [ includeAssociations ],
+    get: [ includeAssociations ],
     create: [],
     update: [],
     patch: [],
@@ -34,3 +35,18 @@ export default {
     remove: []
   }
 };
+
+function includeAssociations(context: HookContext) {
+  const sequelize = context.app.get('sequelizeClient');
+  const { locations, resource } = sequelize.models
+
+  context.params.sequelize = Object.assign(context.params.sequelize || {}, {
+    include: [
+      { model: locations, as: 'location' },
+      { model: resource, as: 'resources' }
+    ],
+    raw: false
+  })
+
+  return context;
+}
