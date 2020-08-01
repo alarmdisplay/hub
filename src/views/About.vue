28f109d09222eb32e0c08025ae33011dca819216
Andreas Brain <dev@abrain.de>
2020-08-01 20:34:25 +0200

    Add links to the project to the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">&Uuml;ber</h1>
+            <p>Website: <a href="https://alarmdisplay.org/">alarmdisplay.org</a></p>
+            <p>Dokumentation: <a href="https://docs.alarmdisplay.org/">docs.alarmdisplay.org</a></p>
+            <p>Community-Forum: <a href="https://community.alarmdisplay.org/">community.alarmdisplay.org</a></p>
+        </div>
+    </section>
 </template>
