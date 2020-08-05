1886fea2627afa2bb37988e099009e5585a33386
Andreas Brain <dev@abrain.de>
2020-08-05 14:26:25 +0200

    Add recent incidents to home page

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import { FeathersVuex } from '@/feathers-client'
 import auth from './store.auth'
 
 import apiKeys from './services/api-keys'
+import incidents from './services/incidents'
 import resources from './services/resources'
 import users from './services/users'
 import watchedFolders from './services/watched-folders'
@@ -21,6 +22,7 @@ export default new Vuex.Store({
   plugins: [
     auth,
     apiKeys,
+    incidents,
     resources,
     users,
     watchedFolders
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,37 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
-    </div>
-  </section>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">Letzte Einsätze</h1>
+            <FeathersVuexFind service="incidents" :query="{ $limit: 5 }" qid="recentIncidents" watch="query">
+                <table class="table is-fullwidth" slot-scope="{ items: incidents }">
+                    <thead>
+                    <tr>
+                        <th>Einsatzgrund</th>
+                        <th>Stichwort</th>
+                        <th>Kategorie</th>
+                        <th>Uhrzeit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <template v-for="incident in incidents">
+                        <tr :key="incident.id">
+                            <th>{{ incident.reason }}</th>
+                            <th>{{ incident.keyword }}</th>
+                            <th>{{ incident.category }}</th>
+                            <td>{{ incident.time }}</td>
+                        </tr>
+                    </template>
+                    </tbody>
+                </table>
+            </FeathersVuexFind>
+        </div>
+    </section>
 </template>
 
 <script>
 
 export default {
   name: 'Home',
-  components: {
-  }
+  components: {}
 }
 </script>
