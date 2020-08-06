fe20c3819c4cdf1fbc619539011ef405210c5dcc
Andreas Brain <dev@abrain.de>
2020-08-06 18:46:59 +0200

    Limit recent incident on the home page to 3 items and show the most recent one at the top

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <section class="section">
         <div class="container">
             <h1 class="title">Letzte Einsätze</h1>
-            <FeathersVuexFind service="incidents" :query="{ $limit: 5 }" qid="recentIncidents" watch="query">
+            <FeathersVuexFind service="incidents" :query="{ $limit: 3, $sort: { time: -1 } }" qid="recentIncidents" watch="query">
                 <ol class="" slot-scope="{ items: incidents }">
                     <li class="media" v-for="incident in incidents" :key="incident.id">
                         <IncidentMediaObject :incident="incident"/>
