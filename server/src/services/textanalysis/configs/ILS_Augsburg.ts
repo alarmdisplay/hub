edafb9ae2d2deaef1a05e143b380e588f6680f74
Andreas Brain <dev@abrain.de>
2020-12-12 15:54:26 +0100

    Make the RegExps more general to accept other characters instead of colons

--- server/src/services/textanalysis/configs/ILS_Augsburg.ts
+++ server/src/services/textanalysis/configs/ILS_Augsburg.ts
@@ -7,8 +7,8 @@ export default {
     {
       beginningMark: /Alarmfax der ILS Augsburg/,
       regexps: [
-        /Absender : (?<sender>.*) Tel/,
-        /Einsatznummer (?:.*): (?<ref>.*)/
+        /Absender [:;=] (?<sender>.*) Tel/,
+        /Einsatznummer (?:.*)[:;=] (?<ref>.*)/
       ]
     },
     {
@@ -20,9 +20,9 @@ export default {
     {
       beginningMark: /EINSATZORT/,
       regexps: [
-        /Straße\s*[:|=](?<loc_street>.*)Haus-Nr\.[:|=]\s*(?<loc_streetnumber>\d+(?:\s?[a-z])?)(?<loc_detail>\s+.*)?$/,
-        /Ort\s*[:|=]\s*(?<loc_zip>\d{5}) (?<loc_city>\w+)/,
-        /Koordinate\s*[:|=]\s(?<loc_gk_x>\d+[,.]\d+) \/ (?<loc_gk_y>\d+[,.]\d+)$/
+        /Straße\s*[:;=](?<loc_street>.*)Haus-Nr\.[:;=]\s*(?<loc_streetnumber>\d+(?:\s?[a-z])?)(?<loc_detail>\s+.*)?$/,
+        /Ort\s*[:;=]\s*(?<loc_zip>\d{5}) (?<loc_city>\w+)/,
+        /Koordinate\s*[:;=]\s(?<loc_gk_x>\d+[,.]\d+) \/ (?<loc_gk_y>\d+[,.]\d+)$/
       ]
     },
     {
@@ -32,8 +32,8 @@ export default {
     {
       beginningMark: /EINSATZGRUND/,
       regexps: [
-        /Schlagw\.[:|=]\s(?<reason>.*)$/,
-        /Stichwort[:|=]\s(?<keyword>.*)$/
+        /Schlagw\.[:;=]\s(?<reason>.*)$/,
+        /Stichwort[:;=]\s(?<keyword>.*)$/
       ]
     },
     {
@@ -45,7 +45,7 @@ export default {
     {
       beginningMark: /BEMERKUNG/,
       regexps: [
-        /Einsatzplan[:|=](?<description>(?:.|\n)*)/m
+        /Einsatzplan[:;=](?<description>(?:.|\n)*)/m
       ]
     }
   ],
