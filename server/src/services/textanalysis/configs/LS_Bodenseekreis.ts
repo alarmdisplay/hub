1b9e2763f3e5c4b4835283ef0f298df4255ab29a
Tobias Bystricky <tobi@tobiby.de>
2021-04-04 22:41:41 +0200

    Updated LS-Bodenseekreis config to real message

--- server/src/services/textanalysis/configs/LS_Bodenseekreis.ts
+++ server/src/services/textanalysis/configs/LS_Bodenseekreis.ts
@@ -2,18 +2,27 @@ import {TextAnalysisConfig} from '../../../declarations'
 
 export default {
   name: 'LS Bodensee-Oberschwaben',
-  beginningMark: /Hallo/,
-  endMark: /!/,
+  beginningMark: /\u0002/,
+  endMark: /\u0003/,
   importantWords: [
-    'Welt',
+    'MSG',
   ],
   sections: [
-    {beginningMark: /Hallo/,
+    {beginningMark: /\u0002/,
       regexps: [
-        /Absender [:;=] (?<sender>.*) Tel/,
-        /Einsatznummer (?:.*)[:;=] (?<ref>.*)/
+        /(?<reason>(\s((.+?)\s){2}))/,
+        /(?<loc_city>\w+,)/,
+        /(?<loc_detail>,\s\w+)/,
+        /(?<=(,\s\w+){1})(?<loc_street>,\s\w+)/,
+        /(?<=(,\s\w+){2})(?<loc_streetnumber>,\s\w+)/,
+        /(?<loc_gk_x>\d+\?\d+'\d+[,.]\d+"),\s.*(?<loc_gk_y>\d+\?\d+'\d+[,.]\d+")/
+
+      ]},
+    {beginningMark: /MSG:/,
+      regexps: [
+        /(?<description>(?:.|\n)*)/m
       ]}
       
   ],
-  triggerWords: ['Hallo']
+  triggerWords: ['\u0002']
 } as TextAnalysisConfig
