390151f91b027b0f36a4083de53aa98d41f63c3c
Andreas Brain <dev@abrain.de>
2021-04-14 14:04:40 +0200

    Keep track of multiple serial monitors and improve error handling

--- server/src/services/serial-monitors/serial-monitors.class.ts
+++ server/src/services/serial-monitors/serial-monitors.class.ts
@@ -15,36 +15,104 @@ interface SerialMonitorsData {
 
 export class SerialMonitors extends Service<SerialMonitorsData> {
   private app: Application
+  private monitors: Map<Number, SerialPort>
 
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
     this.app = app
+    this.monitors = new Map<Number, SerialPort>()
   }
 
   setup (app: Application) {
     // Wait for the database, then start monitoring all serial ports marked as active
     (app.get('databaseReady') as Promise<void>)
-      .then(() => this.find({ query: { active: true }, paginate: false }))
+      .then(() => this.find({ query: { active: true }, paginate: false }) as Promise<SerialMonitorsData[]>)
       .then((serialMonitors) => {
-        if (!Array.isArray(serialMonitors)) {
-          logger.error('Query for serial monitors did not return an Array')
-          return
+        let serialMonitorPromises = serialMonitors.map(serialMonitor => this.startMonitoring(serialMonitor));
+        // @ts-ignore Function allSettled() seems to be unknown
+        return Promise.allSettled(serialMonitorPromises)
+      })
+      .then(results => {
+        for (const result of results) {
+          if (result.status === "fulfilled") {
+            logger.info('Started monitoring %s', result.value.port)
+          } else {
+            logger.error('Could not start monitoring: ', result.reason.message)
+          }
         }
+      })
+      .catch(reason => {
+        logger.error('Could not query serial ports to monitor: ', reason)
+      })
+  }
 
-        Promise.all(serialMonitors.map(serialMonitor => this.startMonitoring(serialMonitor)))
-          .catch(reason => {
-            logger.error('Could not start to monitor serial ports', reason)
-          })
-      }).catch(reason => {
-      logger.error('Could not query serial ports to monitor', reason)
+  private async startMonitoring(serialMonitor: SerialMonitorsData): Promise<SerialMonitorsData> {
+    // If the port is already monitored, stop that first
+    if (this.monitors.has(serialMonitor.id)) {
+      logger.warn('Serial monitor for port %s already running, stopping the old one first ...', serialMonitor.port)
+      await this.stopMonitoring(serialMonitor)
+    }
+
+    return new Promise((resolve, reject) => {
+      let port = new SerialPort(serialMonitor.port, { autoOpen: false })
+      this.monitors.set(serialMonitor.id, port)
+
+      // Register for the open event
+      port.on('open', () => {
+        let parser = port.pipe(new InterByteTimeout({ interval: serialMonitor.timeout }))
+        parser.on('data', (data: any) => this.notifyListeners(data, serialMonitor))
+
+        resolve(serialMonitor)
+      })
+
+      // Set up error logging for unhandled errors
+      port.on('error', error => {
+        logger.error('Serial monitor for %s reports: %s', serialMonitor.port, error)
+      })
+
+      port.on('close', error => {
+        if (error.disconnected) {
+          logger.error('Serial port %s disconnected', serialMonitor.port)
+        }
+        logger.info('Serial monitor for %s stopped', serialMonitor.port)
+      })
+
+      // Open the port and clean up if there has been an error
+      port.open(error => {
+        if (error) {
+          this.monitors.delete(serialMonitor.id)
+          reject(new Error('Could not open serial port: ' + error.message))
+        }
+      })
     })
   }
 
-  private async startMonitoring(serialMonitor: SerialMonitorsData) {
-    let port = new SerialPort(serialMonitor.port)
-    let parser = port.pipe(new InterByteTimeout({ interval: serialMonitor.timeout }))
-    parser.on('data', (data: any) => this.notifyListeners(data, serialMonitor))
-    logger.info('Started monitoring %s', serialMonitor.port)
+  private async stopMonitoring(serialMonitor: SerialMonitorsData): Promise<SerialMonitorsData> {
+    return new Promise((resolve, reject) => {
+      if (!this.monitors.has(serialMonitor.id)) {
+        // No monitor seems to be running for this port
+        resolve(serialMonitor)
+        return
+      }
+
+      let monitor = this.monitors.get(serialMonitor.id)
+      if (!monitor) {
+        // The stored monitor is invalid, just remove it
+        this.monitors.delete(serialMonitor.id)
+        resolve(serialMonitor)
+        return
+      }
+
+      monitor.close(error => {
+        if (error) {
+          reject(new Error(`Error closing port ${serialMonitor.port}: ${error.message}`))
+          return
+        }
+
+        this.monitors.delete(serialMonitor.id)
+        resolve(serialMonitor)
+      })
+    })
   }
 
   private async notifyListeners(alarmText: string, portToWatch: SerialMonitorsData) {
