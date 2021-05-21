64c552941454d45e0c5de678431e1a4d3d080c96
Andreas Brain <dev@abrain.de>
2021-05-21 19:06:39 +0200

    Allow changing the password of a user

--- console/src/components/UserEditor.vue
+++ console/src/components/UserEditor.vue
@@ -16,13 +16,14 @@
                 <input class="input" type="email" id="email" v-model="item.email">
             </p>
         </div>
-        <div class="field" v-if="!isNewItem">
+        <div class="field">
             <label class="label" for="password">
                 Passwort
             </label>
             <p class="control">
-                <input class="input" type="password" id="password" autocomplete="new-password" v-model="item.password">
+                <input class="input" type="password" id="password" autocomplete="new-password" v-model="newPassword">
             </p>
+            <p v-if="!isNewItem" class="help">Wird dieses Feld leer gelassen, bleibt das aktuelle Passwort bestehen. Zur Änderung ein neues Passwort eingeben.</p>
         </div>
         <div class="buttons is-right">
             <button class="button" type="button" @click="$emit('reset')">Zur&uuml;cksetzen</button>
@@ -45,14 +46,22 @@ export default {
       return this.item.id === undefined
     }
   },
+  data() {
+    return {
+      newPassword: ''
+    }
+  },
   methods: {
     isValid: function () {
-      return (this.item.password !== '' || this.isNewItem) && this.item.email !== ''
+      return this.item.email !== '' && ((this.isNewItem && this.newPassword !== '') || !this.isNewItem)
     }
   },
   setup(props, context) {
     function handleSubmit() {
       if (this.isValid()) {
+        if (this.isNewItem || this.newPassword !== '') {
+          this.item.password = this.newPassword
+        }
         context.emit('save')
       } else {
         // TODO show validation result
