640493616778d07cde6a38de90646b94a8ce5137
Andreas Brain <dev@abrain.de>
2021-06-09 13:02:26 +0200

    Point to the new documentation link

--- console/src/views/output/Display.vue
+++ console/src/views/output/Display.vue
@@ -5,7 +5,7 @@
             <p>
                 Die Einsätze können an die Anzeige weitergeleitet werden. Dazu benötigt der Server der Anzeige
                 einen <router-link :to="{ name: 'api-key-list' }">API-Key</router-link>. Das weitere Vorgehen ist in der
-                <a href="https://docs.alarmdisplay.org/de/Anzeige/Installation/Linux.html#page_Mit_der_Alarmzentrale_verbinden">Dokumentation</a> beschrieben.
+                <a href="https://docs.alarmdisplay.org/de/Anzeige/Konfiguration#page_Mit_der_Zentrale_verbinden">Dokumentation</a> beschrieben.
             </p>
         </div>
     </section>
