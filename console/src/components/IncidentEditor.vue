alarmdisplay/hub

commit dc6238b1b57085e4759e5a4c406460f495d5741a

Explicitly reset latitude and longitude properties if fields are empty

console/src/components/IncidentEditor.vue
@@ -253,6 +253,12 @@ export default {
   setup(props, context) {
     function handleSubmit() {
       if (this.isValid()) {
+        if (this.item.location.latitude === '') {
+          this.item.location.latitude = null
+        }
+        if (this.item.location.longitude === '') {
+          this.item.location.longitude = null
+        }
         context.emit('save')
       } else {
         // TODO show validation result

commit 4d8fa2980c30dca7a513eaee527e2def0c053104

Fix bug introduced in dc6238b

console/src/components/IncidentEditor.vue
@@ -253,10 +253,10 @@ export default {
   setup(props, context) {
     function handleSubmit() {
       if (this.isValid()) {
-        if (this.item.location.latitude === '') {
+        if (this.item.location && this.item.location.latitude === '') {
           this.item.location.latitude = null
         }
-        if (this.item.location.longitude === '') {
+        if (this.item.location && this.item.location.longitude === '') {
           this.item.location.longitude = null
         }
         context.emit('save')