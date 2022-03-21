62ba4dc4c2eb3f2815b4839977e7dffc16925eed
Andreas Brain <dev@abrain.de>
2022-03-21 14:42:09 +0100

    Wait for the server status to become ready, before starting the tests

--- test-api/001-wait.js
+++ test-api/001-wait.js
@@ -20,4 +20,23 @@ describe('Wait for server', function () {
         done();
       });
   });
+
+  it(`should be ready`, function(done) {
+    this.timeout(2000);
+    this.slow(5000); // Basically disable the 'slow' reporting
+    chai.request(server.base)
+      .get('/status')
+      .end((err, res) => {
+        // Delay the evaluation a bit, so we don't burn through our retries
+        setTimeout(() => {
+          if (err) {
+            done(err);
+            return
+          }
+          res.should.have.status(200);
+          res.body.should.include({ ready: true });
+          done();
+        }, 1500);
+      });
+  });
 });
