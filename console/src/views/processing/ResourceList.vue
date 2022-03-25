8f232330dd34b2f84cab184d8153afea1331e87f
Andreas Brain <dev@abrain.de>
2022-03-25 21:38:39 +0100

    Add pagination to incident list and resource list

--- console/src/views/processing/ResourceList.vue
+++ console/src/views/processing/ResourceList.vue
@@ -14,23 +14,57 @@
                 </router-link>
             </div>
 
-            <FeathersVuexFind v-slot="{ items: resources }" service="resources" :query="{ $sort: { name: 1 }, $limit: 50 }" qid="resourceList" watch="query">
-                <ul>
-                    <li v-for="resource in resources" :key="resource.id">
-                        <ResourceCard :resource="resource"/>
-                    </li>
-                </ul>
-            </FeathersVuexFind>
+            <FeathersVuexPagination v-model="pagination" :latest-query="latestQuery">
+                <template #default="{ currentPage, pageCount, next, prev, canNext, canPrev }">
+                    <PaginationUI
+                        :current-page="currentPage"
+                        :page-count="pageCount"
+                        :can-prev="canPrev"
+                        :can-next="canNext"
+                        @next="next"
+                        @prev="prev"
+                    />
+                </template>
+            </FeathersVuexPagination>
+
+            <ul>
+                <li v-for="resource in resources" :key="resource.id">
+                    <ResourceCard :resource="resource"/>
+                </li>
+            </ul>
         </div>
     </section>
 </template>
 
 <script>
-  import ResourceCard from '@/components/ResourceCard'
-  export default {
-    name: 'ResourceList',
-    components: { ResourceCard }
+import PaginationUI from '@/components/PaginationUI'
+import ResourceCard from '@/components/ResourceCard'
+import { models, useFind } from 'feathers-vuex'
+import { computed, ref } from '@vue/composition-api'
+
+export default {
+  name: 'ResourceList',
+  components: { ResourceCard, PaginationUI },
+  setup() {
+    const { Resource } = models.api
+
+    const pagination = ref({
+      $limit: 10,
+      $skip: 0
+    })
+
+    const params = computed(() => {
+      const query = { $sort: { name: 1 } }
+      Object.assign(query, pagination.value)
+      return { query, qid: 'resourceList', paginate: true }
+    })
+
+    const data = useFind({ model: Resource, params: params })
+    const { items: resources, latestQuery } = data
+
+    return { resources, pagination, latestQuery }
   }
+}
 </script>
 
 <style scoped>
